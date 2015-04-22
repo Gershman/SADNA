261595caeaa45ce537c279bab9364d86d5edecbf
nirmochiach <nirmochiach@gmail.com>
2015-04-22 16:43:30 +0300

    9

--- ConsoleApplication1/ConsoleApplication1/MailManager.cs
+++ ConsoleApplication1/ConsoleApplication1/MailManager.cs
@@ -14,7 +14,7 @@ namespace ConsoleApplication1
 {
     public class MailManager
     {
-        private const string LOG_FILE_PATH_E = "c:\\stam.txt";
+        private const string LOG_FILE_PATH_T = "c:\\stam.txt";
 
 
         private readonly MySqlUtils sqlUtils;
